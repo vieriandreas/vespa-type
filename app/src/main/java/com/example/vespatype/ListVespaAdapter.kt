11f0cc77ed9b196001dc05cb66bf0829090a5390
vieriandreas <vierinapitupulu7@gmail.com>
2025-02-04 18:02:14 +0700

    fix recyclerView

--- app/src/main/java/com/example/vespatype/ListVespaAdapter.kt
+++ app/src/main/java/com/example/vespatype/ListVespaAdapter.kt
@@ -3,14 +3,17 @@ package com.example.vespatype
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ListVespaAdapter(private val ListVespa: ArrayList<Vespa>): RecyclerView.Adapter<ListVespaAdapter.ListViewHolder>() {
+class ListVespaAdapter(private val listVespa: ArrayList<Vespa>): RecyclerView.Adapter<ListVespaAdapter.ListViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_vespa, parent, false)
         return ListViewHolder(view)
     }
+
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, description, photo) = listVespa[position]
@@ -21,27 +24,10 @@ class ListVespaAdapter(private val ListVespa: ArrayList<Vespa>): RecyclerView.Ad
 
     override fun getItemCount(): Int = listVespa.size
 
-
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
         val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
         val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
-
-
-    }
-
-
 
 }
